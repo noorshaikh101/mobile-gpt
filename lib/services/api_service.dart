3af72dbb2bca8e2b2830421da0ce43e4be244b78
noorshaikh101 <noor101101@gmail.com>
2024-01-03 23:20:11 +0530

    Separate HTTP request logic from conversion logic

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,32 +7,65 @@ import 'package:chatgpt_course/models/chat_model.dart';
 import 'package:chatgpt_course/models/models_model.dart';
 import 'package:http/http.dart' as http;
 
+// class ApiService {
+//   static Future<List<ModelsModel>> getModels() async {
+//     try {
+//       var response = await http.get(
+//         Uri.parse("$BASE_URL/models"),
+//         headers: {'Authorization': 'Bearer $API_KEY'},
+//       );
+
+//       Map jsonResponse = jsonDecode(response.body);
+
+//       if (jsonResponse['error'] != null) {
+//         // print("jsonResponse['error'] ${jsonResponse['error']["message"]}");
+//         throw HttpException(jsonResponse['error']["message"]);
+//       }
+//       // print("jsonResponse $jsonResponse");
+//       List temp = [];
+//       for (var value in jsonResponse["data"]) {
+//         temp.add(value);
+//         // log("temp ${value["id"]}");
+//       }
+//       return ModelsModel.modelsFromSnapshot(temp);
+//     } catch (error) {
+//       log("error $error");
+//       rethrow;
+//     }
+//   }
+
 class ApiService {
   static Future<List<ModelsModel>> getModels() async {
     try {
-      var response = await http.get(
+      final response = await http.get(
         Uri.parse("$BASE_URL/models"),
         headers: {'Authorization': 'Bearer $API_KEY'},
       );
 
-      Map jsonResponse = jsonDecode(response.body);
+      final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
 
       if (jsonResponse['error'] != null) {
-        // print("jsonResponse['error'] ${jsonResponse['error']["message"]}");
-        throw HttpException(jsonResponse['error']["message"]);
+        throw ApiException(jsonResponse['error']["message"]);
       }
-      // print("jsonResponse $jsonResponse");
-      List temp = [];
-      for (var value in jsonResponse["data"]) {
-        temp.add(value);
-        // log("temp ${value["id"]}");
-      }
-      return ModelsModel.modelsFromSnapshot(temp);
+
+      final List<Map<String, dynamic>> data = List.from(jsonResponse["data"]);
+      return ModelsModel.modelsFromSnapshot(data);
     } catch (error) {
-      log("error $error");
+      logger.error("Error in getModels: $error");
       rethrow;
     }
   }
+}
+
+class ApiException implements Exception {
+  final String message;
+
+  ApiException(this.message);
+
+  @override
+  String toString() => 'ApiException: $message';
+}
+
 
   // Send Message using ChatGPT API
   static Future<List<ChatModel>> sendMessageGPT(
